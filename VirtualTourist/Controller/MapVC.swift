e03ce1013f4b1d754917f1ba273429aa6c26e066
Sarah Estrellado <sarah.estrellado@icloud.com>
2019-06-19 10:39:54 -0500

    removed unused funcs in AppDelegate and changed pin appearance from bubbles to pins

--- VirtualTourist/Controller/MapVC.swift
+++ VirtualTourist/Controller/MapVC.swift
@@ -88,4 +88,18 @@ extension MapVC: MKMapViewDelegate {
         }
     }
     
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let reuseId = "pin"
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.pinTintColor = .red
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
+    }
+    
 }
